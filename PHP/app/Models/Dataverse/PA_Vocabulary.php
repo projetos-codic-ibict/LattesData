dcc6fdd0ee3704ededb193b4850ee9e6de91b94e
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2022-04-27 17:39:14 -0300

    Update PA_Vocabulary.php

--- PHP/app/Models/Dataverse/PA_Vocabulary.php
+++ PHP/app/Models/Dataverse/PA_Vocabulary.php
@@ -120,7 +120,7 @@ class PA_Vocabulary extends Model
                     $sx .= $sep.round(trim($line['vc_order']));
                     $sx .= "\n";
                 }
-            $sx = substr($sx,0,strlen($sx)-1);
+            $sx = substr($sx,0,strlen($sx)-2);
             return $sx;
         }      
 
