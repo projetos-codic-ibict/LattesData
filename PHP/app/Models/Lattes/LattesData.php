80dedfb08f71be5cf9434bfd21a33f9665e07e11
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2022-05-16 14:14:57 -0300

    Update LattesData.php

--- PHP/app/Models/Lattes/LattesData.php
+++ PHP/app/Models/Lattes/LattesData.php
@@ -333,16 +333,14 @@ class LattesData extends Model
 				$Dataverse = new \App\Models\Dataverse\Dataverse();
 				//$sx .= $this->modPQ($dt,$id);
 				$dd = $this->modPQ($dt, $id);
-				echo "1";
 
 				/****************************************** USER */
 				$user = $this->getUser($dt);	
-				echo "2";
 
 				/***************************** CHAMADA DATAVERSE */
 				$chamada = $this->getChamada($dt,$user);
 				$Dataverse->CreateDataverse($chamada,'beneficiarios');
-				echo "=================3";
+				pre($dt);
 				$parent = $dt['alias'];				
 
 				/***************************** PROJETO DATAVERSE */
