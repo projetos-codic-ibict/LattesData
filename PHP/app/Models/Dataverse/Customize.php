9ed65161fd33cf5740b0abb4687cdb30fdb4293c
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2022-05-19 06:16:02 -0300

    Update Customize.php

--- PHP/app/Models/Dataverse/Customize.php
+++ PHP/app/Models/Dataverse/Customize.php
@@ -105,7 +105,7 @@ class Customize extends Model
 								echo cr();
 								$sx .= "======================== Copy files =".$langs[$r].cr();
 								$n_subdir = $subdir[$r];
-								if ($langs[$r] == 'en')
+								if ($langs[$r] == 'br')
 									{
 										$dir = '/var/www/dataverse/langTmp/pt_BR/dataverse-language-packs-develop/'.$n_subdir;
 										$out = '/var/www/dataverse/langTmp/';
