1d25cd2bb490643893765d49231c21e33916fe39
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2022-07-15 10:28:26 -0300

    v0.22.07.15

--- PHP/app/Models/Dataverse/Datasets.php
+++ PHP/app/Models/Dataverse/Datasets.php
@@ -40,60 +40,63 @@ class Datasets extends Model
 	protected $beforeDelete         = [];
 	protected $afterDelete          = [];
 
-	function CreateDatasets($dd='',$dataset='',$parent)	
-		{
-			$sx = h(lang('dataverse.carlos_chagas_cnpq'));
-			$DataverseAPI = new \App\Models\Dataverse\API();
+	function management($d1 = '', $d2 = '')
+	{
+		echo "===" . $d1;
+	}
 
-			$url = $this->url.'api/dataverses/'.$parent.'/datasets';
-			$id = $dd['id'];
-	
-			$json = json_encode($dataset, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-			$file = '.tmp/datasets/dataset_'.$id.'.json';
-			file_put_contents($file, $json);
+	function CreateDatasets($dd = '', $dataset = '', $parent)
+	{
+		$sx = h(lang('dataverse.carlos_chagas_cnpq'));
+		$DataverseAPI = new \App\Models\Dataverse\API();
 
-			$dd['AUTH'] = true;
-			$dd['POST'] = true;
-			$dd['FILE'] = $file;
-			$dd['api'] = 'api/dataverses/'.$parent.'/datasets';
-			$dd['url'] = $_ENV['DATAVERSE_URL'];
-			$dd['apikey'] = $_ENV['DATAVERSE_APIKEY'];
+		$url = $this->url . 'api/dataverses/' . $parent . '/datasets';
+		$id = $dd['id'];
+
+		$json = json_encode($dataset, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+		$file = '.tmp/datasets/dataset_' . $id . '.json';
+		file_put_contents($file, $json);
+
+		$dd['AUTH'] = true;
+		$dd['POST'] = true;
+		$dd['FILE'] = $file;
+		$dd['api'] = 'api/dataverses/' . $parent . '/datasets';
+		$dd['url'] = $_ENV['DATAVERSE_URL'];
+		$dd['apikey'] = $_ENV['DATAVERSE_APIKEY'];
 
 
-			$rst = $DataverseAPI->curlExec($dd);
-			$rsp = json_decode($rst,true);
+		$rst = $DataverseAPI->curlExec($dd);
+		$rsp = json_decode($rst, true);
 
-			if (isset($rsp['status']))
-				{
-					//$email = $dd['user']['email'];
-					//$nome = $dd['user']['firstName'].' '.$dataset['user']['lastName'];
-					if (isset($rsp['data']['persistentId']))
-					{
-						$DOI = $rsp['data']['persistentId'];
-						//$sx .= 'Prezado(a) '.$nome;
-						//$sx .= '<p>Foram enviadas instruções para o e-mail '.$$email.'</p>';					
-						$url = getenv("DATAVERSE_URL").'/dataset.xhtml?persistentId='.$DOI.'&version=DRAFT';
-						$sx .= 'Link para acesso <a href="'.$url.'"><tt>'.$url.'</tt></a>';
-						echo '<pre>';
-						print_r($rsp);
-					} else {
-						pre($rsp);
-						$sx .= "ERRO: ".$rsp;
-					}
-					echo $sx;
-					return $sx;
-				} 
-			else
-				{
-					$sx = 'ERRO: FALHA NA CONEXÃO COM O DATAVERSE';
-					return $sx;
-				}
-			if (!isset($rsp['status'])) { return ""; }
-			$sx = bsmessage(h($rsp['status']).$rsp['message'],1);
-			return $sx;
-			exit;
-			
-			$sta = $rsp['status'];			
-			return $sx;
+		if (isset($rsp['status'])) {
+			//$email = $dd['user']['email'];
+			//$nome = $dd['user']['firstName'].' '.$dataset['user']['lastName'];
+			if (isset($rsp['data']['persistentId'])) {
+				$DOI = $rsp['data']['persistentId'];
+				//$sx .= 'Prezado(a) '.$nome;
+				//$sx .= '<p>Foram enviadas instruções para o e-mail '.$$email.'</p>';
+				$url = getenv("DATAVERSE_URL") . '/dataset.xhtml?persistentId=' . $DOI . '&version=DRAFT';
+				$sx .= 'Link para acesso <a href="' . $url . '"><tt>' . $url . '</tt></a>';
+				echo '<pre>';
+				print_r($rsp);
+			} else {
+				pre($rsp);
+				$sx .= "ERRO: " . $rsp;
 			}
+			echo $sx;
+			return $sx;
+		} else {
+			$sx = 'ERRO: FALHA NA CONEXÃO COM O DATAVERSE';
+			return $sx;
+		}
+		if (!isset($rsp['status'])) {
+			return "";
+		}
+		$sx = bsmessage(h($rsp['status']) . $rsp['message'], 1);
+		return $sx;
+		exit;
+
+		$sta = $rsp['status'];
+		return $sx;
+	}
 }
