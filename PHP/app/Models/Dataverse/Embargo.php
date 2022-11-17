5317b5ca4b69c89078062672db669d141cba172d
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2022-11-17 06:14:02 -0300

    Update Embargo.php

--- PHP/app/Models/Dataverse/Embargo.php
+++ PHP/app/Models/Dataverse/Embargo.php
@@ -48,8 +48,13 @@ class Embargo extends Model
 				$sx .= $this->enable($d2, $d3);
 				break;
 
+			case 'remove_embargo':
+				$sx .= $this->remove_embargo($d2, $d3);
+				break;
+
 			default:
 				$menu[PATH . MODULE . 'dataverse/embargo/enable'] = 'dataverse.Embargo.enable';
+				$menu[PATH . MODULE . 'dataverse/embargo/remove_embargo'] = 'dataverse.Embargo.remove_embargo';
 				$sx .= menu($menu);
 		}
 		return $sx;
@@ -62,6 +67,20 @@ class Embargo extends Model
 		$sx = 'curl -X PUT -d 24 '.$url.'/api/admin/settings/:MaxEmbargoDurationInMonths';
 		$sx .= '<br>';
 		$sx .= '<b>24</b> - ';
+		return $sx;
+	}
+
+	function remove_embargo()
+	{
+		$Dataverse = new \App\Models\Dataverse\index();
+		$url = $Dataverse->server();
+		$sx = 'curl -X PUT -d 24 ' . $url . '/api/admin/settings/:MaxEmbargoDurationInMonths';
+		$sx .= 'export API_TOKEN=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx</br>';
+		$sx .= 'export SERVER_URL=https: //demo.dataverse.org</br>';
+		$sx .= 'export PERSISTENT_IDENTIFIER=doi:10.5072/FK2/7U7YBV</br>';
+		$sx .= 'export JSON=\'{"fileIds":[300,301]}</br>\'';
+		$sx .= '</br>';
+		$sx .= 'curl -H "X-Dataverse-key: $API_TOKEN" -H "Content-Type:application/json" "$SERVER_URL/api/datasets/:persistentId/files/actions/:unset-embargo?persistentId=$PERSISTENT_IDENTIFIER" -d "$JSON"</br>';
 		return $sx;
 	}
 }
